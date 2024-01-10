98a6c32ab86255cb9fe17510c2c6a492d89a7eb8
dkawalecc <dkawalec08@gmail.com>
2024-01-10 13:30:49 +0100

    Movable pieces

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,47 @@ import { OrbitControls, Stats, Environment } from '@react-three/drei'
 import { Chessboard } from './components/Chessboard'
 import { Room } from './components/SmokingRoom'
 import { Lights } from './components/Lights'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
   const [running, setRunning] = useState(false)
-  
-  // <Chessboard position={[0,-0.1,0]}/>
-  // <Room position={[0,-35,0]} scale={[26,26,26]}/>
+  const [restart, setRestart] = useState(0)
+  const controlsRef = useRef()
+  // useEffect(()=> {
+  //   setRunning(false)
+  // }, [restart])
   return (
     <>
-    <Canvas camera={{near: 0.00001, position: [1, 0.8, 1.2], }}>
-    <OrbitControls autoRotate={!running} autoRotateSpeed={1.6}/>
-    <Lights/>
-    <Chessboard position={[-0.087797,-0.1,-0.009988]}/>
-    <Environment preset='apartment' background/>
+    <Canvas ref={controlsRef} camera={{near: 0.00001, position: [1, 0.8, 1.2], }}>
+      <OrbitControls autoRotate={!running} autoRotateSpeed={1.8} dampingFactor={0.15}/>
+      {/* <Lights/> */}
+      <Chessboard key={restart} position={[-0.087797,-0.1,-0.009988]} onPointerMissed={()=>{console.log("missed")}}/>
+      {/* <Room position={[0,-25,0]} scale={[20,20,20]}/> */}
+      <Environment preset='apartment' background/>
       <Stats />
-      <axesHelper/>
-      </Canvas>
-      <span 
-        className='btn--start'
-        onClick={(e) => {
-          setRunning(true)
-          e.target.style.display = "none"
-        }}
-      >Start the game</span>
-    </>
+      {/* <axesHelper/> */}
+    </Canvas>
+      {!running ?
+        <>
+          <span 
+          className='btn--start'
+          onClick={(e) => {
+            setRunning(true)
+            // e.target.style.display = "none"
+          }}
+          >Start the game</span>
+          <div className='background'></div>
+        </>
+        :
+        <span
+          className='btn--restart'
+          onClick={()=>{
+            setRestart(prev => prev+1)
+            console.log(controlsRef)
+          }}
+        >Restart</span>
+      }
+        </>
   )
 }
 
---
+++ src/components/Pieces/Rook.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
+
+import React from 'react'
+import { useRef, useState } from 'react'
+import {  useFrame, useThree } from '@react-three/fiber'
+
+export function Rook({pref, material, hoverMaterial, ...props}) { 
+  const mesh = useRef()
+  const { viewport } = useThree()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(0)
+  // useFrame(() => (pref.current.rotation.z = pref.current.rotation.x += 0.01))
+  
+  
+  return (
+      <mesh 
+        {...props}
+        ref={pref}
+        material={hovered || active ? hoverMaterial: material}
+        // scale={(viewport.width / 5) * (active ? 1.5 : 1)}
+        // onClick={(e) => {
+        //     setActive((prev) => prev+10)
+        //     // console.log(pref)
+        //   }
+        // }
+        onPointerOver={(e) => {
+          e.stopPropagation()
+          setHover(true)
+          }}
+        onPointerOut={(e) => setHover(false)} 
+        />
+
+      )
+  }
